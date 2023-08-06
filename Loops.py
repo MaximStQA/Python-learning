8b815b6da797deee2b4d9b8311b1ae995b321dcf
Subacunogaara <subacunogaara1989@gmail.com>
2023-08-06 16:45:28 +0200

    enumerate

--- Loops.py
+++ Loops.py
@@ -11,20 +11,20 @@
 #
 # print(summ)
 
-login = 'Sasha'
-password = '123'
-in_log = input('Введите логин: ')
-while True:
-    if in_log != login:
-        print('Вы ввели неверный логин')
-        in_log = input('Введите логин: ')
-        continue
-    else:
-        pasw = input('Введите пароль: ')
-        if pasw != password:
-            print('Вы ввели неверный пароль')
-            in_log = ''
-            continue
+# login = 'Sasha'
+# password = '123'
+# in_log = input('Введите логин: ')
+# while True:
+#     if in_log != login:
+#         print('Вы ввели неверный логин')
+#         in_log = input('Введите логин: ')
+#         continue
+#     else:
+#         pasw = input('Введите пароль: ')
+#         if pasw != password:
+#             print('Вы ввели неверный пароль')
+#             in_log = ''
+#             continue
 
 # my_list = [1, 2, 3, 3, 3, 4, 5, 6, 6, 7]
 #
@@ -60,6 +60,63 @@ while True:
 #     print('Done')
 
 
+# t = ['a', 'b', 'v', 'g', 'd', 'e', 'zh', 'z', 'i', 'y', 'k', 'l',
+#      'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch',
+#      'sh', 'shch', '', 'y', '', 'e', 'yu', 'ya']
+#
+# start_index = ord('а') # русский алфавит
+# title = 'Саша, Комаров!?'
+# result = ''
+#
+# for l in title.lower():
+#     if 'а' <= l <= 'я':
+#         result += t[ord(l) - start_index]
+#     elif l == 'ё':
+#         result += 'yo'
+#     else:
+#         result += l
+#
+# print(result)
 
 
+# print(list(range(5)))
+# for i in range(2, 5):
+#     print(i)
+# else:
+#     print('first end')
+#
+# for i in range(5):
+#     print(i)
+#     break
+# else:
+#     print('second end')
 
+
+# if flag == True:
+#     pass
+# else:
+#     error
+
+# for i in range(5):
+#     if i == 3:
+#         continue
+#
+#     print(f'next step {i}')
+# else:
+#     print('happy end')
+
+# my_list = ['a', 'b', 'c', 'd']
+# for ind, value in enumerate(my_list):
+#     print(f'значение {value} по индексу {ind}')
+
+"""Поменять местами наибольший и наименьший элементы списка."""
+
+# my_list = [2, 1, 2, 3, 4, 5, 6, 7]
+# x = max(my_list)
+# e = min(my_list)
+# for i, v in enumerate(my_list):
+#     if v == x:
+#         my_list[i] = e
+#     elif v == e:
+#         my_list[i] = x
+# print(my_list)
