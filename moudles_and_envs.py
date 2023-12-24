5833cadb4c9e29f7c8a8679c0164fef71b555099
Subacunogaara <subacunogaara1989@gmail.com>
2023-12-24 13:59:18 +0100

    modules

---
+++ moudles_and_envs.py
@@ -0,0 +1,20 @@
+# import math
+import random
+import pygame
+# import pprint
+from math import sqrt
+from math import *
+from pprint import pprint as print
+
+
+a = 2
+# pprint.pprint(locals())
+print(locals())
+
+""" для создания виртуального окружения - python -m venv maks_hw 
+    для активации maks_hw\Scripts\activate
+    pip list - показать список библиотек установленных в конкретное окружение в терминале
+    pip freeze > requirements.txt - создать файл со списком установленных библиотек
+    pip install -r requirements.txt - установить все библиотеки из файла requirements.txt в текущее виртуальное окружение
+
+"""
